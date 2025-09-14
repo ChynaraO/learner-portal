15ebb0d83c364cf06373178e3b13ddd920f1a62a
ChynaraO <omurzakova_chyna@mail.ru>
2025-09-14 16:38:32 -0500

    Initial commit: Spring Boot project with .gitignore

---
+++ src/main/java/com/learners/learner_portal/LearnerPortalApplication.java
@@ -0,0 +1,13 @@
+package com.learners.learner_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnerPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnerPortalApplication.class, args);
+	}
+
+}
